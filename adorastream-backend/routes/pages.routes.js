3f8c60d8dc5306cc91dfe24afb03590fb8f2fcf4
Adoram Shoval <s1adoram@gmail.com>
2025-10-15 14:37:49 +0300

    add requireLogin middleware to pages router to avoid repeating same code

--- adorastream-backend/routes/pages.routes.js
+++ adorastream-backend/routes/pages.routes.js
@@ -5,20 +5,20 @@ const router = express.Router();
 // Resolve static HTML files, for example page(login) --> <path>/login.html
 const page = (name) => path.join(__dirname, '../..', 'public', `${name}.html`);
 
-router.get('/', (req, res) => {
-  if (req.session?.user?.id) {
-    return res.redirect('/profile-selection');
-  }
+function requireLogin(req, res, next) {
+  if (req.session?.user?.id) return next();
   return res.redirect('/login');
+}
+
+router.get('/', requireLogin, (req, res) => {
+  return res.redirect('/profile-selection');
 });
 
-router.get('/profile-selection', (req, res, next) => {
-  if (!req.session?.user?.id) return res.redirect('/login');
+router.get('/profile-selection', requireLogin, (req, res, next) => {
   res.sendFile(page('profile-selection'));
 });
 
-router.get('/add-profile', (req, res, next) => {
-  if (!req.session?.user?.id) return res.redirect('/login');
+router.get('/add-profile', requireLogin, (req, res, next) => {
   res.sendFile(page('add-profile'));
 });
 
